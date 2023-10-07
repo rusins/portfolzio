54ace563418f24c98b91fe506bf4afc95d4146af
raitis <raitis.krikis@gmail.com>
2023-10-07 23:56:34 +0300

    Fix emoji favicon dupe

--- src/main/scala/portfolzio/website/html/Templates.scala
+++ src/main/scala/portfolzio/website/html/Templates.scala
@@ -19,11 +19,7 @@ object Templates:
       meta(charsetAttr := "utf-8"),
       meta(nameAttr := "viewport", contentAttr := "width=device-width, initial-scale=1.0"),
       title("Raitis Kriķis Photography"),
-      // Camera emoji as favicon
-      link(
-        relAttr := "icon",
-        hrefAttr := "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>📷\uD83D\uDCF7</text></svg>",
-      ),
+      link(relAttr := "icon", hrefAttr := "/img/favicon.svg"),
       // Include Unpoly before your own stylesheets and JavaScripts
       link(relAttr := "stylesheet", hrefAttr := "/css/unpoly.min.css"),
       link(relAttr := "stylesheet", hrefAttr := "/css/pure-min.css"),
